9326ca48c2d8b6c776e4c3a9003a0c32da448499
Noah Schooler, Robert Lee and Sheila Stephen <noah@>
2018-11-05 11:55:10 -0800

    add form for users to input post information

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,15 @@ function Post (name, header, post) {
 Post.prototype.createPost = function() {
   $("#results").append("<div class='container well'><h2>" + this.header + "</h2>" + "<br>" + this.post + "<br>" + this.name + "</div>")
 }
+
+$(document).ready(function(){
+  $("#add-post").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var header = $("#header").val();
+    var post = $("#post").val();
+
+    var newPost = new Post(name, header, post)
+    newPost.createPost();
+  });
+});
