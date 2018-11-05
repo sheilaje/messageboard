481cd2c9b51712719c865b3f31dae33b0c056141
Noah Schooler, Robert Lee and Sheila Stephen <sheila@>
2018-11-05 14:31:10 -0800

    Add reply post button event listener

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,15 @@ function Post (name, header, post) {
 }
 
 Post.prototype.createPost = function() {
-  $("#results").append("<div class='container well'><h2>" + this.header + "</h2>" + "<br>" + this.post + "<br>" + this.name + "</div>")
+  $("#results").append("<div class='container well'><h2>" + this.header + "</h2>" + "<br>" + this.post + "<br>" + this.name + "<br><button id='reply-post-1' type='button' class='btn' value=1>Reply post</button></div>")
 }
 
 $(document).ready(function(){
+  $("#results").on("click", "button", function() {
+    console.log("hey, i see your button click on relpies, you clicked on" + this.id);
+//    var testValue = $("#results #reply-post-1").val();
+  });
+
   $("#add-post").submit(function(event) {
     event.preventDefault();
     var name = $("#name").val();
