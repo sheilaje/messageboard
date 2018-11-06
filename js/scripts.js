26eeb8e2f53258fa998d46236753e0d34e64bf4e
Noah Schooler, Robert Lee and Sheila Stephen <noah@>
2018-11-06 09:33:01 -0800

    add modal box for post input

--- js/scripts.js
+++ js/scripts.js
@@ -57,6 +57,28 @@ Post.prototype.createPost = function() {
 }
 
 $(document).ready(function(){
+  //Modal code
+  var modal = document.getElementById("post-modal");
+  var btn = document.getElementById("add-post-button");
+  var span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  //End modal code
+
   $("#results").on("click", ".btn-reply-post", function(e) {
     console.log("hey, i see your button click on relpies, you clicked on" + this.id);
     console.log("hey this is your text you are replying to" + this.post);
