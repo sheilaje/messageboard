1c8e8b4fe282361abb605de03be5418160f7101a
Sheila Stephen <sheila.je@gmail.com>
2018-11-06 00:12:26 -0800

    make teh reply post function work

--- js/scripts.js
+++ js/scripts.js
@@ -14,28 +14,31 @@ Topic.prototype.assignId = function() {
   return this.currentId;
 }
 
-Topic.prototype.findPost = function(id) {
-  for (var i=0; i < this.posts.length; i ++) {
-    if (this.posts[i].id === id) {
-      return this.posts[i];
-    }
-  };
-  return false;
-}
-
-Topic.prototype.replyToPost = function(id) {
-  for (var i=0; i < this.posts.length; i ++) {
-    if (this.posts[i].id === id) {
-      return this.posts[i];
-    }
-  };
-  return false;
-}
-
 function Post (name, header, post) {
   this.name = name,
   this.header = header,
   this.post = post
+}
+
+
+function createReplyLink(postId,replyId) {
+  return "<div id='reply-div-" + postId+ replyId + "'><button data-postid='" + postId + "' data-id='" + replyId + "' type='button' class='btn btn-reply-post'>Reply post</button></div>";
+}
+
+
+function createReplyTextArea(postId, replyId) {
+  return "<div class='well' id='reply-msg-" + postId + replyId + "'>" +
+"<div class='form-group'><label for='name'>Name:</label>" +
+"<input id='replyname' class='form-control' type='text' placeholder='Enter Your Name'></div>" +
+"<div class='form-group'><label for='header'>Reply Message:</label>" +
+"<input id='replymsg' class='form-control' type='text' placeholder='Enter Your Reply'></div>" +
+"<button data-postid='" + postId + "' data-id='" + replyId + "' type='button' class='btn btn-reply-submit'>Reply post</button></div>";
+}
+
+function displayReply(id, replyname, replymessage){
+  var theCurrentTime = new Date();
+
+  return "Name: "+ replyname + "<br>Time" + theCurrentTime.toDateString() + "<br>Message" + replymessage + "<br>";
 }
 
 var index = 1;
@@ -49,18 +52,41 @@ var topicsObject = new Topic();
 // newTopic.addPost(post1);
 // newTopic.addPost(post2);
 
-Post.prototype.createPost = function(postObject) {
+Post.prototype.createPost = function() {
   var theCurrentTime = new Date();
 
-  $("#results").append("<div class='container well'><h2>" + this.header + "</h2>" + "<br>" + this.post + "<br>" + this.name + "<br>" + theCurrentTime.toDateString() + "<br><button id='reply-post-" + topicsObject.currentId + "' type='button' class='btn'>Reply post</button></div>")
+  $("#results").append("<div class='container well' id='first-post-"  + topicsObject.currentId + "'><h2>" + this.header + "</h2>" + "<br>" + this.post + "<br>" + this.name + "<br>" + theCurrentTime.toDateString() + createReplyLink(topicsObject.currentId,0) + "</div>")
 
 }
 
 $(document).ready(function(){
-  $("#results").on("click", "button", function() {
+  $("#results").on("click", ".btn-reply-post", function(e) {
     console.log("hey, i see your button click on relpies, you clicked on" + this.id);
-//    var testValue = $("#results #reply-post-1").val();
     console.log("hey this is your text you are replying to" + this.post);
+    var postId = $(e.target).attr("data-postid");
+    var replyId = $(e.target).attr("data-id");
+    console.log("clicked id=" + postId);
+
+    $("#reply-div-" + postId+replyId).html(createReplyTextArea(postId, replyId));
+
+
+  });
+
+  $("#results").on("click", ".btn-reply-submit", function(e) {
+
+    console.log("Replying to the post" + this.post);
+    var replyId = $(e.target).attr("data-id");
+    var postId = $(e.target).attr("data-postid");
+    console.log("clicked id= postId=" + replyId + " " + postId);
+    var replyName = $("#replyname").val();
+    var replyMessage = $("#replymsg").val();
+
+    $("#reply-msg-" + postId + replyId).html(displayReply(replyId, replyName, replyMessage));
+    var nextId = parseInt(replyId) + 1;
+    $("#first-post-" + postId).append(createReplyLink(postId,nextId));
+  });
+
+  $("#results").on("click", ".btn-delete-post", function() {
   });
 
   $("#add-post").submit(function(event) {
@@ -77,4 +103,5 @@ $(document).ready(function(){
     $("#header").val("");
     $("#post").val("");
   });
+
 });
