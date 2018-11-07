29de81ac4759d91f4a4293080319c7ceb431b2f3
Noah Schooler, Robert Lee and Sheila Stephen <sheila@>
2018-11-07 15:42:35 -0800

    add ability to nest comments

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,15 @@ Topic.prototype.assignId = function() {
   return this.currentId;
 }
 
+Topic.prototype.findPost = function(id) {
+  for (var i=0; i < this.posts.length; i ++) {
+    if (this.posts[i].id === id) {
+      return this.posts[i];
+    }
+  };
+  return false;
+}
+
 Topic.prototype.findName = function(name){
   var tempArray = [];
   for (var i=0; i<this.posts.length; i++){
@@ -34,6 +43,23 @@ function Post (name, header, post) {
   this.header = header,
   this.post = post,
   this.replies = []
+  this.currentId = 0
+}
+
+Post.prototype.pushReply = function(reply) {
+  reply.id = this.assignId();
+  this.replies.push(reply)
+}
+
+Post.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+function Reply (name, post) {
+  this.name = name,
+  this.post = post,
+  this.replies = []
 }
 
 Post.prototype.addReply = function (replyObjectPost) {
@@ -43,28 +69,32 @@ Post.prototype.addReply = function (replyObjectPost) {
 //creates the initial post with a new topic
 Post.prototype.createPost = function() {
   var theCurrentTime = new Date();
-$("#results").append("<div class='container well style-post' id='first-post-"  + topicsObject.currentId + "'><h2>" + this.header + "</h2>" + "<br>" + this.post + "<br>" + this.name + "<br>" + theCurrentTime.toDateString() + createReplyLink(topicsObject.currentId,0) + "</div>")
+  $("#results").append("<div class='container well style-post' id='first-post-" + topicsObject.currentId + "'><h2>" + this.header + "</h2>" + this.post + "<br>" + this.name + "<br>" + theCurrentTime.toDateString() + createReplyLink(topicsObject.currentId,0) + "</div>")
 }
 
 //function createReplyLink creates the Reply Post button dynamically after every post
-function createReplyLink(postId,replyId) {
+function createReplyLink(postId, replyId) {
   return "<div id='reply-div-" + postId+ replyId + "'><button data-postid='" + postId + "' data-id='" + replyId + "' type='button' class='btn btn-default btn-reply-post'>Reply post</button></div>";
 }
 
 //function createReplyTextArea creates the form on the DOM with the fields userName and space for the user to reply to the post
 function createReplyTextArea(postId, replyId) {
   return "<div class='well style-reply' id='reply-msg-" + postId + replyId + "'>" +
-"<div class='form-group'><label for='name'>Name:</label>" +
-"<input id='replyname' class='form-control' type='text' placeholder='Enter Your Name'></div>" +
-"<div class='form-group'><label for='header'>Reply Message:</label>" +
-"<input id='replymsg' class='form-control' type='text' placeholder='Enter Your Reply'></div>" +
-"<button data-postid='" + postId + "' data-id='" + replyId + "' type='button' class='btn btn-reply-submit'>Reply post</button></div>";
+  "<div class='form-group'><label for='name'>Name:</label>" +
+  "<input id='replyname' class='form-control' type='text' placeholder='Enter Your Name'></div>" +
+  "<div class='form-group'><label for='header'>Reply Message:</label>" +
+  "<input id='replymsg' class='form-control' type='text' placeholder='Enter Your Reply'></div>" +
+  "<button data-postid='" + postId + "' data-id='" + replyId + "' type='button' class='btn btn-reply-submit'>Reply post</button></div>";
 }
 
 //function displayReply displays the reply in a new well
-function displayReply(id, replyname, replymessage){
+function displayReply(id, replyname, replymessage, e){
   var theCurrentTime = new Date();
-  return "Name: "+ replyname + "<br>Time" + theCurrentTime.toDateString() + "<br>Message" + replymessage + "<br>";
+  var postId = $(e.target).attr("data-postid");
+  var replyId = $(e.target).attr("data-id");
+  // $("#reply-msg-00").append(createReplyLink(postId, replyId))
+  // $("#first-post-0").append(createReplyLink(postId, replyId))
+  return "Name: "+ replyname + "<br>Time: " + theCurrentTime.toDateString() + "<br>Message: " + replymessage + "<br>" + createReplyLink(postId, replyId + 1);
 }
 
 var topicsObject = new Topic();
@@ -72,30 +102,33 @@ var topicsObject = new Topic();
 //User Interface Logic
 $(document).ready(function(){
 
-//Event Listener for button button reply post
+  //Event Listener for button button reply post
   $("#results").on("click", ".btn-reply-post", function(e) {
     var postId = $(e.target).attr("data-postid");
     var replyId = $(e.target).attr("data-id");
     $("#reply-div-" + postId+replyId).html(createReplyTextArea(postId, replyId));
-});
+  });
 
-//event Listener for button reply submit
-$("#results").on("click", ".btn-reply-submit", function(e) {
-  var replyId = $(e.target).attr("data-id");
-  console.log($("data-id"));
-  var postId = $(e.target).attr("data-postid");
-  var replyName = $("#replyname").val();
-  var replyMessage = $("#replymsg").val();
+  //event Listener for button reply submit
+  $("#results").on("click", ".btn-reply-submit", function(e) {
+    var replyId = $(e.target).attr("data-id");
+    var postId = $(e.target).attr("data-postid");
+    console.log("you are here!! reply ID: " + replyId);
+    var replyName = $("#replyname").val();
+    var replyMessage = $("#replymsg").val();
 
-  var newReply = new Post(replyName, "", replyMessage) //This is a temp reply post object
-  topicsObject.posts[postId].replies.push(newReply);
+    var newReply = new Post(replyName,  "", replyMessage) //This is a temp reply post object
+    topicsObject.posts[postId].replies.push(newReply);
 
-  $("#reply-msg-" + postId + replyId).html(displayReply(replyId, replyName, replyMessage));
-  var nextId = parseInt(replyId) + 1;
-  $("#first-post-" + postId).append(createReplyLink(postId,nextId));
-});
+    $("#reply-msg-" + postId + replyId).html(displayReply(replyId, replyName, replyMessage, e));
+    var nextId = parseInt(replyId) + 1;
+    console.log("you are there!! next ID: " + nextId);
+    if (replyId === 1) {
+      $("#first-post-" + postId).append(createReplyLink(postId,nextId));
+    }
+  });
 
-//Event Listener for button search post
+  //Event Listener for button search post
   $("#searchid").on("click", "#search-post", function(event){
     event.preventDefault();
     var searchName = $("#search").val();
